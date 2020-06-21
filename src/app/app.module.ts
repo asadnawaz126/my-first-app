493bc98f3e8445d785cd0efb806d81499a54e41e
Asad Nawaz <asadnawazmobilecrazy@gmail.com>
2020-06-21 15:22:05 -0400

    modified app component files to work with user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
