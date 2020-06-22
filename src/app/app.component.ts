c25e43daa4b5bd755284fdda36c5827ad3a45448
Asad Nawaz <asadnawazmobilecrazy@gmail.com>
2020-06-21 20:44:26 -0400

    assg3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Asad';
+  buttonText = 'Hide Paragraph';
+  clickCount = 0;
+  clicks = []
+
+  changeButtonText(){
+    this.clickCount+=1;
+    this.clicks.push(this.clickCount);
+    // console.log(this.clicks);
+    if(this.buttonText === 'Hide Paragraph'){
+      console.log("in ig")
+      this.buttonText = 'Show Paragraph';
+    }
+    else if(this.buttonText === 'Show Paragraph'){
+      console.log("in else")
+      this.buttonText = 'Hide Parahraph';
+    }
+    //this.buttonText === 'Hide Paragraph' ? 'Show Paragraph' : 'Hide Paragraph';
+    console.log(this.buttonText)
+  }
 }
